6932e42fefec19ee15efb4eab400c95af489d1ad
Martin Mokry <martin-kokos@users.noreply.github.com>
2023-02-22 15:53:49 +0100

    Switch to FastAPI

--- app.py
+++ app.py
@@ -13,191 +13,245 @@ import uuid
 import traceback
 import json
 from utils.reapi import ReAPI
+from functools import lru_cache
 
-routes = web.RouteTableDef()
+from fastapi import FastAPI
+from fastapi import Header, Query, Request
+from fastapi.responses import HTMLResponse, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="/app/templates")
 
 
-async def fetch_remote_data(app):
-    try:
-        while True:
-            try:
-                # clients update
-                ips = ["ingest-readsb:150"]
-                print("Fetching data from", ips)
-                clients = []
-                receivers = []
-                for ip in ips:
-                    async with app["session"].get(f"http://{ip}/clients.json") as resp:
-                        data = await resp.json()
-                        clients += data["clients"]
-                        print(len(clients), "clients")
+class ApiUuidRequest(BaseModel):
+    version: str
 
-                    async with app["session"].get(
-                        f"http://{ip}/receivers.json"
+
+class Provider(object):
+
+    def __init__(self):
+        self.beast_clients = set()
+        self.beast_receivers = []
+        self.mlat_sync_json = {}
+        self.mlat_totalcount_json = {}
+        self.mlat_clients = {}
+        self.ReAPI = ReAPI("http://reapi-readsb:30152/re-api/")
+
+    async def startup(self):
+        self.client_session = await aiohttp.ClientSession(
+            raise_for_status=True,
+            timeout=aiohttp.ClientTimeout(total=5.0, connect=1.0, sock_connect=1.0),
+        )
+        self.bg_taks = asyncio.create_task(self.fetch_remote_data())
+
+    async def shutdown(self):
+        await self.bg_task.cancel()
+        await self.client_session.close()
+
+    async def fetch_remote_data(self):
+        try:
+            while True:
+                try:
+                    # clients update
+                    ips = ["ingest-readsb:150"]
+                    print("Fetching data from", ips)
+                    clients = []
+                    receivers = []
+                    for ip in ips:
+                        async with self.client_session.get(f"http://{ip}/clients.json") as resp:
+                            data = await resp.json()
+                            clients += data["clients"]
+                            print(len(clients), "clients")
+
+                        async with self.client_session.get(
+                            f"http://{ip}/receivers.json"
+                        ) as resp:
+                            data = await resp.json()
+                            for receiver in data["receivers"]:
+                                lat, lon = round(receiver[8], 2), round(receiver[9], 2)
+                                receivers.append([lat, lon])
+                    print(len(receivers), "receivers")
+
+                    self.beast_clients = beast_clients_to_set(clients)
+                    self.beast_receivers = receivers
+
+                    # mlat update
+                    print("Fetching mlat data")
+                    async with self.client_session.get(
+                        "http://mlat-mlat-server:150/sync.json"
                     ) as resp:
                         data = await resp.json()
-                        for receiver in data["receivers"]:
-                            lat, lon = round(receiver[8], 2), round(receiver[9], 2)
-                            receivers.append([lat, lon])
-                print(len(receivers), "receivers")
+                    print("Fetched mlat sync.json")
+                    self.mlat_sync_json = self.anonymize_mlat_data(data)
+                    self.mlat_totalcount_json = {
+                        "0A": len(self.mlat_sync_json),
+                        "UPDATED": datetime.now().strftime("%a %b %d %H:%M:%S UTC %Y"),
+                    }
 
-                app["beast_clients"] = beast_clients_to_set(clients)
-                app["beast_receivers"] = receivers
+                    # mlat clients.json
+                    print("Fetching mlat clients.json")
+                    async with self.client_session.get(
+                        "http://mlat-mlat-server:150/clients.json"
+                    ) as resp:
+                        data = await resp.json()
+                    self.mlat_clients = data
 
-                # mlat update
-                print("Fetching mlat data")
-                async with app["session"].get(
-                    "http://mlat-mlat-server:150/sync.json"
-                ) as resp:
-                    data = await resp.json()
-                print("Fetched mlat sync.json")
-                app["mlat_sync_json"] = anonymize_mlat_data(app, data)
-                app["mlat_totalcount_json"] = {
-                    "0A": len(app["mlat_sync_json"]),
-                    "UPDATED": datetime.now().strftime("%a %b %d %H:%M:%S UTC %Y"),
-                }
-
-                # mlat clients.json
-                print("Fetching mlat clients.json")
-                async with app["session"].get(
-                    "http://mlat-mlat-server:150/clients.json"
-                ) as resp:
-                    data = await resp.json()
-                app["mlat_clients"] = data
-
-                print("Looped..")
-                await asyncio.sleep(1)
-            except Exception as e:
-                traceback.print_exc()
-                print("Error in background task:", e)
-    except asyncio.CancelledError:
-        print("Background task cancelled")
+                    print("Looped..")
+                    await asyncio.sleep(1)
+                except Exception as e:
+                    traceback.print_exc()
+                    print("Error in background task:", e)
+        except asyncio.CancelledError:
+            print("Background task cancelled")
 
 
-def beast_clients_to_set(clients):
-    clients_set = set()
-    for client in clients:
-        hex = client[0]
-        ip = client[1].split()[1]
-        kbps = client[2]
-        conn_time = client[3]
-        msg_s = client[4]
-        position_s = client[5]
-        reduce_signal = client[6]
-        positions = client[8]
+    @staticmethod
+    def beast_clients_to_set(clients):
+        clients_set = set()
+        for client in clients:
+            hex = client[0]
+            ip = client[1].split()[1]
+            kbps = client[2]
+            conn_time = client[3]
+            msg_s = client[4]
+            position_s = client[5]
+            reduce_signal = client[6]
+            positions = client[8]
 
-        clients_set.add(
-            (hex, ip, kbps, conn_time, msg_s, position_s, reduce_signal, positions)
+            clients_set.add(
+                (hex, ip, kbps, conn_time, msg_s, position_s, reduce_signal, positions)
+            )
+        return clients_set
+
+
+    @staticmethod
+    def mlat_clients_to_list(clients, ip=None):
+        clients_list = []
+        keys_to_copy = (
+            "user privacy connection peer_count bad_sync_timeout outlier_percent".split()
         )
-    return clients_set
+        for name, client in clients.items():
+            print(client)
+            if ip is not None and client["source_ip"] != ip:
+                continue
+            clients_list.append({key: client[key] for key in keys_to_copy if key in client})
+        return clients_list
 
 
-def mlat_clients_to_list(clients, ip=None):
-    clients_list = []
-    keys_to_copy = (
-        "user privacy connection peer_count bad_sync_timeout outlier_percent".split()
-    )
-    for name, client in clients.items():
-        print(client)
-        if ip is not None and client["source_ip"] != ip:
-            continue
-        clients_list.append({key: client[key] for key in keys_to_copy if key in client})
-    return clients_list
+    def anonymize_mlat_data(self, data):
+        sanitized_data = {}
+        for name, value in data.items():
+            sanitised_peers = {}
+            for peer, peer_value in value["peers"].items():
+                sanitised_peers[self.cachehash(peer)] = peer_value
+
+            sanitized_data[self.cachehash(name)] = {
+                "lat": value["lat"],
+                "lon": value["lon"],
+                "peers": sanitised_peers,
+            }
+
+        return sanitized_data
 
 
-def anonymize_mlat_data(app, data):
-    sanitized_data = {}
-    for name, value in data.items():
-        sanitised_peers = {}
-        for peer, peer_value in value["peers"].items():
-            sanitised_peers[cachehash(app, peer)] = peer_value
+    @staticmethod
+    def get_clients_per_ip(clients, ip: str) -> list:
+        return [client for client in clients if client[1] == ip]
 
-        sanitized_data[cachehash(app, name)] = {
-            "lat": value["lat"],
-            "lon": value["lon"],
-            "peers": sanitised_peers,
-        }
-
-    return sanitized_data
-
-
-def get_clients_per_ip(clients, ip: str) -> list:
-    return [client for client in clients if client[1] == ip]
-
-
-def cachehash(app, name):
-    # Only hash UUIDs
-    try:
-        uuid.UUID(name)
-        salt = b"$2b$04$OGq0aceBoTGtzkUfT0FGme"
-        if name not in app["mlat_cached_names"]:
-            print("Hashing...")
-            hash = bcrypt.hashpw(name.encode(), salt).decode()
-            candidate = "".join([c for c in hash if c.isalnum()])[-13:]
-            app["mlat_cached_names"][name] = name[0:3] + "_" + candidate[-13:]
-    except ValueError:
-        return name
-
-    return app["mlat_cached_names"][name]
+    @lru_cache(maxsize=1024)
+    def cachehash(self, name):
+        # Only hash UUIDs
+        try:
+            uuid.UUID(name)
+            salt = b"$2b$04$OGq0aceBoTGtzkUfT0FGme"
+            _hash = bcrypt.hashpw(name.encode(), salt).decode()
+            candidate = "".join([c for c in _hash if c.isalnum()])[-13:]
+            name_id = name[0:3] + "_" + candidate[-13:]
+            return name_id
+        except ValueError:
+            print(f'Unable to hash {name[:4]}...')
+            return name
 
 
-@routes.get("/")
-async def index(request):
-    ip = request.headers["X-Original-Forwarded-For"]
-    clients_beast = get_clients_per_ip(request.app["beast_clients"], ip)
-    clients_mlat = mlat_clients_to_list(request.app["mlat_clients"], ip)
+provider = Provider()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await provider.startup()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await provider.shutdown()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request : Request, x_original_forwarded_for : str | None = Header(default=None)):
+    """
+    Return a template index.html with the clients.
+    """
+    client_ip = x_original_forwarded_for
+    clients_beast = provider.get_clients_per_client_ip(provider.beast_clients, client_ip)
+    clients_mlat = provider.mlat_clients_to_list(provider.mlat_clients, client_ip)
     context = {
         "clients_beast": clients_beast,
         "clients_mlat": clients_mlat,
         "own_mlat_clients": len(clients_mlat),
-        "ip": ip,
-        "len_beast": len(request.app["beast_clients"]),
-        "len_mlat": len(request.app["mlat_clients"]),
+        "ip": client_ip,
+        "len_beast": len(provider.beast_clients),
+        "len_mlat": len(provider.mlat_clients),
     }
-    response = aiohttp_jinja2.render_template("index.html", request, context)
+    response = templates.TemplateResponse("index.html", {"request": request, "context": context})
     return response
 
-    # Return a template index.html with the clients, pass the clients to the template which is a index.html file
+
+@app.get("/api/0/receivers")
+async def receivers():
+    return provider.beast_receivers
 
 
-@routes.get("/api/0/receivers")
-async def receivers(request):
-    return web.json_response(request.app["beast_receivers"])
+@app.get("/api/0/mlat-server/0A/sync.json")
+async def mlat_receivers():
+    return provider.mlat_sync_json
 
 
-@routes.get("/api/0/mlat-server/0A/sync.json")
-async def mlat_receivers(request):
-    return web.json_response(request.app["mlat_sync_json"])
+@app.get("/api/0/mlat-server/totalcount.json")
+async def mlat_totalcount_json():
+    return provider.mlat_totalcount_json
 
 
-@routes.get("/api/0/mlat-server/totalcount.json")
-async def mlat_totalcount_json(request):
-    return web.json_response(app["mlat_totalcount_json"])
-
-
-@routes.post("/api/0/uuid")
-async def post_uuid(request):
-    data = await request.json()
+@app.post("/api/0/uuid")
+async def post_uuid(data: ApiUuidRequest):
     generated_uuid = str(uuid.uuid4())
     json_log = json.dumps({"uuid": generated_uuid, "data": data})
     print(json_log)
-    return web.json_response({"uuid": generated_uuid})
+    return {"uuid": generated_uuid}
 
 
-@routes.get("/metrics")
-async def metrics(request):
+@app.get("/metrics")
+async def metrics():
+    """
+    Return metrics for Prometheus
+    """
     metrics = [
-        "adsb_api_beast_total_receivers {}".format(len(request.app["beast_receivers"])),
-        "adsb_api_beast_total_clients {}".format(len(request.app["beast_clients"])),
-        "adsb_api_mlat_total {}".format(len(request.app["mlat_sync_json"])),
+        "adsb_api_beast_total_receivers {}".format(len(provider.beast_receivers)),
+        "adsb_api_beast_total_clients {}".format(len(provider.beast_clients)),
+        "adsb_api_mlat_total {}".format(len(provider.mlat_sync_json)),
     ]
-    return web.Response(text="\n".join(metrics))
+    return Response(content="\n".join(metrics), media_type='text/plain')
 
 
-@routes.get("/api/0/me")
-async def api_me(request):
-    ip = request.headers["X-Original-Forwarded-For"]
-    beast_clients_set = get_clients_per_ip(request.app["beast_clients"], ip)
+@app.get("/api/0/me")
+async def api_me(x_original_forwarded_for : str | None = Header(default=None)):
+    client_ip = x_original_forwarded_for
+    beast_clients_set = provider.get_clients_per_client_ip(provider.beast_clients, client_ip)
     beast_clients_list = []
     for client in beast_clients_set:
         beast_clients_list.append(
@@ -211,7 +265,7 @@ async def api_me(request):
                 "positions_per_second": client[5],
             }
         )
-    mlat_clients = mlat_clients_to_list(request.app["mlat_clients"], ip)
+    mlat_clients = provider.mlat_clients_to_list(provider.mlat_clients, client_ip)
     response = {
         "feeding": {
             "beast": len(beast_clients_list) > 0,
@@ -221,28 +275,36 @@ async def api_me(request):
             "beast": beast_clients_list,
             "mlat": mlat_clients,
         },
-        "ip": ip,
+        "client_ip": client_ip,
     }
     # Format the response pretty
     return web.json_response(response, dumps=lambda x: json.dumps(x, indent=4))
 
 
-@routes.get("/v2/{generic:pia|mil|ladd|all}")
-async def v2_generic(request):
-    generic = request.match_info["generic"]
+@app.get("/v2/{generic}")
+async def v2_generic(
+        generic: str = Query(default=..., regex='pia|mil|ladd|all'),
+        x_original_forwarded_for : str | None = Header(default=None),
+        ):
+    client_ip = x_original_forwarded_for
+
     allowed = {
         "pia": ["all", "filter_pia"],
         "mil": ["all", "filter_mil"],
         "ladd": ["all", "filter_ladd"],
         "all": ["all"],
     }
-    res = await request.app["ReAPI"].request(allowed[generic], request)
+    res = await provider.ReAPI.request(params=allowed[generic], client_ip=client_ip)
     return web.json_response(res)
 
 
-@routes.get("/v2/{generic:squawk|type|reg|hex|callsign}/{filter}")
-async def v2_generic_filter(request):
-    generic, filter = request.match_info["generic"], request.match_info["filter"]
+@app.get("/v2/{generic}/{filter}")
+async def v2_generic_filter(generic: str = Query(default=..., regex='squawk|type|reg|hex|callsign'),
+                            _filter: str = Query(default=..., alias='filter'),
+                            x_original_forwarded_for : str | None = Header(default=None),
+                            ):
+    client_ip = x_original_forwarded_for
+
     # Fix that so it is a list
     allowed = {
         "squawk": ["all", f"filter_squawk={filter}"],
@@ -251,58 +313,18 @@ async def v2_generic_filter(request):
         "hex": [f"find_hex={filter}"],
         "callsign": [f"find_callsign={filter}"],
     }
-    res = await request.app["ReAPI"].request(allowed[generic], request)
+    res = await provider.ReAPI.request(params=allowed[generic], client_ip=client_ip)
     return web.json_response(res)
 
 
-@routes.get("/v2/point/{lat}/{lon}/{radius}")
-async def v2_point(request):
-    lat, lon, radius = (
-        request.match_info["lat"],
-        request.match_info["lon"],
-        min(int(request.match_info["radius"]), 250),
-    )
-    res = await request.app["ReAPI"].request([f"circle={lat},{lon},{radius}"], request)
+@app.get("/v2/point/{lat}/{lon}/{radius}")
+async def v2_point(lat: float, lon: float, radius: int):
+    radius = min(radius, 250)
+
+    res = await provider.ReAPI.request(params=[f"circle={lat},{lon},{radius}"], client_ip=client_ip)
     return web.json_response(res)
-
-
-async def background_tasks(app):
-    app["fetch_remote_data"] = asyncio.create_task(fetch_remote_data(app))
-
-    yield
-
-    app["fetch_remote_data"].cancel()
-    await app["fetch_remote_data"]
-
-
-# aiohttp server
-app = web.Application()
-app.add_routes(routes)
-app["beast_clients"] = set()
-app["beast_receivers"] = []
-app["mlat_sync_json"] = {}
-app["mlat_totalcount_json"] = {}
-app["mlat_cached_names"] = {}
-app["mlat_clients"] = {}
-app["ReAPI"] = ReAPI("http://reapi-readsb:30152/re-api/")
-
-
-async def aiohttp_session_setup(app):
-    timeout = aiohttp.ClientTimeout(total=5.0, connect=1.0, sock_connect=1.0)
-    app["session"] = aiohttp.ClientSession(timeout=timeout)
-
-
-async def aiohttp_session_cleanup(app):
-    await app["session"].close()
-    asyncio.sleep(0)
-
-
-# add background task
-app.cleanup_ctx.append(background_tasks)
-app.on_startup.append(aiohttp_session_setup)
-app.on_cleanup.append(aiohttp_session_cleanup)
 
 
 if __name__ == "__main__":
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("/app/templates"))
-    web.run_app(app, host="0.0.0.0", port=80)
+    print('Run with:')
+    print('uvicorn app:app --host 0.0.0.0 --port 80')
