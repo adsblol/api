e98929c61c046a0d0dbbbe228d235b8af6170c21
k <k@example.com>
2023-02-23 13:20:25 +0100

    Improve API docs

--- utils/api_v2.py
+++ utils/api_v2.py
@@ -1,21 +1,28 @@
-from fastapi import APIRouter, Header, Query
+from fastapi import APIRouter, Header, Query, Path
+
 
 from . import provider
 from .models import PrettyJSONResponse, V2Response_Model
+from typing import Any
 
 router = APIRouter(
     prefix="/v2",
     tags=["v2"],
+    responses={200: {"model": V2Response_Model}},
 )
 
-@router.get("/pia", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+
+@router.get(
+    "/pia",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts with PIA addresses (Privacy ICAO Address)",
+    description="Returns all aircraft with [PIA](https://nbaa.org/aircraft-operations/security/privacy/privacy-icao-address-pia/) addresses.",
+)
 async def v2_pia(
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns all aircraft with [PIA](https://nbaa.org/aircraft-operations/security/privacy/privacy-icao-address-pia/) addresses.
-    """
+
     client_ip = x_original_forwarded_for
     params = ["all", "filter_pia"]
 
@@ -23,14 +30,17 @@ async def v2_pia(
     return res
 
 
-@router.get("/mil", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/mil",
+    response_class=PrettyJSONResponse,
+    summary="Military registered aircrafts",
+    description="Returns all military registered aircraft.",
+)
 async def v2_mil(
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns all military registered aircraft.
-    """
+
     client_ip = x_original_forwarded_for
     params = ["all", "filter_mil"]
 
@@ -38,14 +48,17 @@ async def v2_mil(
     return res
 
 
-@router.get("/ladd", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/ladd",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts on LADD (Limiting Aircraft Data Displayed)",
+    description="Returns all aircrafts on [LADD](https://www.faa.gov/pilots/ladd) filter.",
+)
 async def v2_ladd(
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns all aircrafto on [LADD](https://www.faa.gov/pilots/ladd) filter.
-    """
+
     client_ip = x_original_forwarded_for
     params = ["all", "filter_ladd"]
 
@@ -53,30 +66,19 @@ async def v2_ladd(
     return res
 
 
-@router.get("/all", response_class=PrettyJSONResponse, response_model=V2Response_Model)
-async def v2_all(
-    x_original_forwarded_for: str
-    | None = Header(default=None, include_in_schema=False),
-) -> V2Response_Model:
-    """
-    Returns all [aircraft](https://en.wikipedia.org/wiki/Aircraft).
-    """
-    client_ip = x_original_forwarded_for
-    params = ["all"]
-
-    res = await provider.ReAPI.request(params=params, client_ip=client_ip)
-    return res
-
-
-@router.get("/squawk/{squawk}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/squawk/{squawk}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts with specific squawk (1200, 7700, etc.)",
+    description='Returns aircraft filtered by "squawk" [transponder code](https://en.wikipedia.org/wiki/List_of_transponder_codes).',
+)
 async def v2_squawk_filter(
-    squawk: str = Query(default=..., example="1200"),
+    # Allow custom examples
+    squawk: str = Path(default=..., example="1200"),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns aircraft filtered by "squawk" [transponder code](https://en.wikipedia.org/wiki/List_of_transponder_codes).
-    """
+
     client_ip = x_original_forwarded_for
     params = ["all", f"filter_squawk={squawk}"]
 
@@ -84,15 +86,18 @@ async def v2_squawk_filter(
     return res
 
 
-@router.get("/type/{aircraft_type}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/type/{aircraft_type}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts of specific type (A320, B738)",
+    description="Returns aircraft filtered by [aircraft type designator code](https://en.wikipedia.org/wiki/List_of_aircraft_type_designators).",
+)
 async def v2_type_filter(
-    aircraft_type: str = Query(default=..., example="A332"),
+    aircraft_type: str = Path(default=..., example="A320"),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns aircraft filtered by [aircraft type designator code](https://en.wikipedia.org/wiki/List_of_aircraft_type_designators).
-    """
+
     client_ip = x_original_forwarded_for
     params = [f"find_type={aircraft_type}"]
 
@@ -100,15 +105,18 @@ async def v2_type_filter(
     return res
 
 
-@router.get("/reg/{registration}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/reg/{registration}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts with specific registration (G-KELS)",
+    description="Returns aircraft filtered by [aircarft registration code](https://en.wikipedia.org/wiki/Aircraft_registration).",
+)
 async def v2_reg_filter(
-    registration: str = Query(default=..., example="G-KELS"),
+    registration: str = Path(default=..., example="G-KELS"),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns aircraft filtered by [aircarft registration code](https://en.wikipedia.org/wiki/Aircraft_registration).
-    """
+
     client_ip = x_original_forwarded_for
     params = [f"find_reg={registration}"]
 
@@ -116,15 +124,18 @@ async def v2_reg_filter(
     return res
 
 
-@router.get("/hex/{icao_hex}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/hex/{icao_hex}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts with specific transponder hex code (4CA87C)",
+    description="Returns aircraft filtered by [transponder hex code](https://en.wikipedia.org/wiki/Aviation_transponder_interrogation_modes#ICAO_24-bit_address).",
+)
 async def v2_hex_filter(
-    icao_hex: str = Query(default=..., example="4CA87C"),
+    icao_hex: str = Path(default=..., example="4CA87C"),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns aircraft filtered by [transponder hex code](https://en.wikipedia.org/wiki/Aviation_transponder_interrogation_modes#ICAO_24-bit_address).
-    """
+
     client_ip = x_original_forwarded_for
     params = [f"find_hex={icao_hex}"]
 
@@ -132,15 +143,17 @@ async def v2_hex_filter(
     return res
 
 
-@router.get("/callsign/{callsign}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/callsign/{callsign}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts with specific callsign (JBU1942)",
+    description="Returns aircraft filtered by [callsign](https://en.wikipedia.org/wiki/Aviation_call_signs).",
+)
 async def v2_callsign_filter(
-    callsign: str = Query(default=..., example="JBU1942"),
+    callsign: str = Path(default=..., example="JBU1942"),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Returns aircraft filtered by [callsign](https://en.wikipedia.org/wiki/Aviation_call_signs).
-    """
     client_ip = x_original_forwarded_for
     params = [f"find_callsign={callsign}"]
 
@@ -148,21 +161,40 @@ async def v2_callsign_filter(
     return res
 
 
-@router.get("/point/{lat}/{lon}/{radius}", response_class=PrettyJSONResponse, response_model=V2Response_Model)
+@router.get(
+    "/point/{lat}/{lon}/{radius}",
+    response_class=PrettyJSONResponse,
+    summary="Aircrafts surrounding a point (lat, lon) up to 250nm",
+    description="Returns aircraft located in a circle described by the latitude and longtidude of its center and its radius.",
+)
 async def v2_point(
-    lat: float,
-    lon: float,
-    radius: int,
+    lat: float = Path(..., example=40.78),
+    lon: float = Path(..., example=73.97),
+    radius: int = Path(..., example=250),
     x_original_forwarded_for: str
     | None = Header(default=None, include_in_schema=False),
 ) -> V2Response_Model:
-    """
-    Return aircraft located in a circle described by the latitude and longtidude of it's center and it's radius.
-    """
     radius = min(radius, 250)
     client_ip = x_original_forwarded_for
 
     res = await provider.ReAPI.request(
         params=[f"circle={lat},{lon},{radius}"], client_ip=client_ip
     )
+    return res
+
+@router.get(
+    "/all",
+    response_class=PrettyJSONResponse,
+    summary="All aircrafts",
+    description="Returns all [aircraft](https://en.wikipedia.org/wiki/Aircraft).",
+)
+async def v2_all(
+    x_original_forwarded_for: str
+    | None = Header(default=None, include_in_schema=False),
+) -> V2Response_Model:
+
+    client_ip = x_original_forwarded_for
+    params = ["all"]
+
+    res = await provider.ReAPI.request(params=params, client_ip=client_ip)
     return res
