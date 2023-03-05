17a8be9ea70129c4d8db92612052c0e99b9cdfb6
Katia <katia@linux.com>
2023-03-05 17:01:59 +0100

    Fix orjson

--- utils/reapi.py
+++ utils/reapi.py
@@ -1,6 +1,7 @@
 import re
 
 import aiohttp
+import orjson
 
 
 class ReAPI:
@@ -30,7 +31,7 @@ class ReAPI:
         async with aiohttp.ClientSession(timeout=timeout) as session:
             async with session.get(url) as response:
                 response.raise_for_status()
-                return await response.text()
+                return orjson.loads((await response.text()))
 
 
 if __name__ == "__main__":
