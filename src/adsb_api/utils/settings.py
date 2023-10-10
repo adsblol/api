35ba92930385ac05a0252966457bb718ec6e6d95
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2023-10-10 13:08:06 +0100

    Update settings.py
    Also this can go if not used
    
    Signed-off-by: Mark Pentler <27921983+mpentler@users.noreply.github.com>

--- src/adsb_api/utils/settings.py
+++ src/adsb_api/utils/settings.py
@@ -1,9 +1,5 @@
 import os
 
-SALT_MY = os.environ.get("ADSBLOL_API_SALT_MY")
-SALT_MLAT = os.environ.get("ADSBLOL_API_SALT_MLAT")
-SALT_BEAST = os.environ.get("ADSBLOL_API_SALT_BEAST")
-INSECURE = os.getenv("ADSBLOL_INSECURE") is not None
 ENDPOINTS = os.getenv("ADSBLOL_ENDPOINTS", "").split(",")
 REDIS_HOST = os.getenv("ADSBLOL_REDIS_HOST", "redis://redis")
 REDIS_TTL = int(os.getenv("ADSBLOL_REDIS_TTL", "5"))
