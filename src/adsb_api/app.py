8bd6fa20df83aa54b88cf918aa0406c9a9ab6bc6
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2023-10-10 16:07:49 +0100

    Update app.py
    Woops. We need this. 
    
    Signed-off-by: Mark Pentler <27921983+mpentler@users.noreply.github.com>

--- src/adsb_api/app.py
+++ src/adsb_api/app.py
@@ -21,6 +21,7 @@ from redis import asyncio as aioredis
 from adsb_api.utils.api_routes import router as routes_router
 from adsb_api.utils.dependencies import browser, redisVRS
 from adsb_api.utils.models import ApiUuidRequest, PrettyJSONResponse
+from adsb_api.utils.settings import REDIS_HOST
 
 PROJECT_PATH = pathlib.Path(__file__).parent.parent.parent
 
