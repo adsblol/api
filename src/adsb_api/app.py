92eb86b6bd7e71a16732a8af39dca5db50f82a41
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2023-10-10 16:52:26 +0100

    Update app.py
    None of this seems to be required
    
    Signed-off-by: Mark Pentler <27921983+mpentler@users.noreply.github.com>

--- src/adsb_api/app.py
+++ src/adsb_api/app.py
@@ -3,7 +3,6 @@ import pathlib
 import secrets
 import time
 import traceback
-import uuid
 import random
 import asyncio
 from collections import defaultdict
@@ -19,7 +18,7 @@ from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
 from adsb_api.utils.api_routes import router as routes_router
-from adsb_api.utils.dependencies import browser, redisVRS
+from adsb_api.utils.dependencies import redisVRS
 from adsb_api.utils.models import ApiUuidRequest, PrettyJSONResponse
 from adsb_api.utils.settings import REDIS_HOST
 
@@ -68,30 +67,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory=PROJECT_PATH / "templates")
 
 
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse("static/favicon.ico")
-
-
 @app.on_event("startup")
 async def startup_event():
     redis = aioredis.from_url(REDIS_HOST, encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="api")
     redisVRS.redis_connection_string = REDIS_HOST
-    await provider.startup()
     await redisVRS.connect()
     await redisVRS.dispatch_background_task()
-    try:
-        await browser.start()
-    except:
-        traceback.print_exc()
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    await provider.shutdown()
     await redisVRS.shutdown()
-    await browser.shutdown()
 
 
 if __name__ == "__main__":
