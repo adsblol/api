1b149cad9b5c0284ccab3c0db759ea6a1ab4c98a
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2023-10-10 16:04:19 +0100

    Update settings.py
    Don't need this getenv
    
    Signed-off-by: Mark Pentler <27921983+mpentler@users.noreply.github.com>

--- src/adsb_api/utils/settings.py
+++ src/adsb_api/utils/settings.py
@@ -1,5 +1,5 @@
 import os
 
 ENDPOINTS = os.getenv("ADSBLOL_ENDPOINTS", "").split(",")
-REDIS_HOST = os.getenv("ADSBLOL_REDIS_HOST", "redis://redis")
+REDIS_HOST = "redis://redis"
 REDIS_TTL = int(os.getenv("ADSBLOL_REDIS_TTL", "5"))
